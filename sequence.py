4a9f775f65447b09ea4b8ac3fd2e8e80772330e5
Joseph <96990909+Woodenman23@users.noreply.github.com>
2022-05-12 20:16:38 -0700

    Add files via upload

---
+++ sequence.py
@@ -0,0 +1,55 @@
+
+import _tkinter as tk
+
+from helpers import *
+from tkinter import *
+
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+
+        yoga_pose = sequence()
+
+        # intialise label
+        text = Label(self, text="".join(yoga_pose), fg="Green", font=("Helvetica", 18))
+        text.place(x=40,y=0)
+        #text.pack()
+        
+
+        # initialise exit button
+        self.pack(fill=BOTH, expand=1)
+        exitButton = Button(self, text="Exit", command=self.clickExitButton)
+        exitButton.place(x=0, y=0)
+
+    # exit button callback
+    def clickExitButton(self):
+        exit()
+
+def sequence():
+    return("Sun Salutation \n",
+    standing(),
+    standing(), "\n",
+    fold(), "\n",
+    twist(), "\n",
+    backbend(), "\n",
+    seated(), "\n",
+    prone(), "\n",
+    supine(), "\n",
+    "Shavasana")
+
+
+
+# intitialise tkinter     
+root = Tk()
+app = Window(root)
+
+# window size
+root.geometry("700x500")
+
+
+# set window title
+root.wm_title("Window")
+
+# show window
+root.mainloop()
